b5c71db19e5f734384f91f77dd09e7072515aa84
josh-bone <jbone@bu.edu>
2021-04-15 13:59:14 -0400

    Decrease frequency of printing

--- drive_scraping.py
+++ drive_scraping.py
@@ -2,14 +2,25 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
 user_handles = pd.read_csv('Twitter_user_handles_to_predict.csv')
-print(user_handles.head)
 
-tweets_list = []
+# loop through twitter handles
+for user in user_handles['Username']:
+    print()
+    print(f"Reading tweets of {user}")
 
-for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:JayHolz410').get_items()):
-    if i > 100:
-        break
-    tweets_list.append([tweet.content])
+    tweets_list = []
+    for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:'+user).get_items()):
 
-for tweet in tweets_list:
-    print(tweet)
+        # Monitor progress
+        if i % 500 == 0:
+            print(tweet.content)
+
+        # TODO: only save the tweets if they contain relevant keywords
+        # TODO: decide on what tobacco-related keywords we want to monitor
+        tweets_list.append([tweet.username, tweet.content, tweet.date])
+
+    cur_df = pd.DataFrame(tweets_list, columns=['Username','Text','Date'])
+    prev_df = pd.read_csv("Scraped_tweets.csv")
+    final_df = pd.concat([prev_df, cur_df]).drop_duplicates().reset_index(drop=True)
+
+    final_df.to_csv("Scraped_tweets.csv")
